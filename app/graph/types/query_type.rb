c8d2b1650984109c6860dcc9aba27df6d6be4664
Jason Haruska <jason@haruska.com>
2017-05-09 11:55:11 -0500

    use built in Node field helpers with custom resolve blocks

--- app/graph/types/query_type.rb
+++ app/graph/types/query_type.rb
@@ -3,33 +3,21 @@ module Types
     name "Query"
     description "The query root of this schema"
 
-    field :node do
-      type GraphQL::Relay::Node.interface
-      description "Fetches an object given its ID."
-      argument :id, !types.ID, "ID of the object."
-
+    field :node, (GraphQL::Relay::Node.field do
       resolve lambda { |_obj, args, context|
         object = ApplicationRecord.global_find(args[:id])
         object && context[:current_ability].can?(:read, object) ? object : nil
       }
+    end)
 
-      relay_node_field(true)
-    end
-
-    field :nodes do
-      type !types[GraphQL::Relay::Node.interface]
-      description("Fetches a list of objects given a list of IDs.")
-      argument :ids, !types[!types.ID], "IDs of the objects."
-
+    field :nodes, (GraphQL::Relay::Node.plural_field do
       resolve lambda { |_obj, args, context|
         args[:ids].map do |id|
           object = ApplicationRecord.global_find(id)
           object && context[:current_ability].can?(:read, object) ? object : nil
         end
       }
-
-      relay_nodes_field(true)
-    end
+    end)
 
     field :viewer do
       type !ViewerType
