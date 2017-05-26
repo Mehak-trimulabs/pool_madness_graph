12b2dac76e91d9388d162e91f6577724d4c677c7
Jason Haruska <jason@haruska.com>
2017-05-26 16:44:04 -0500

    autoload path change

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module PoolMadness
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    config.autoload_paths << Rails.root.join("lib")
+    config.autoload_paths += [Rails.root.join("lib").to_s]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
