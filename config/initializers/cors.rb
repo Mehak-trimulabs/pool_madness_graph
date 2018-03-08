62ccecd7738cc9c2e04873ab3ed3333f605eb190
Jason Haruska <jason@haruska.com>
2018-03-07 20:19:36 -0600

    restrict cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,9 +1,9 @@
 # config/initializers/cors.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins  "*" # "localhost:3000", "staging.pool-madness.com", "pool-madness.com", "www.pool-madness.com"
+    origins "localhost:3000", "staging.pool-madness.com", "pool-madness.com", "www.pool-madness.com"
     resource "*",
              headers: :any,
-             methods: %i[post options]
+             methods: %i[post]
   end
 end
