5c9106a4b6b6a640985e61f0e9b7a7ef8d12b589
Jason Haruska <jason@haruska.com>
2018-03-16 10:14:15 -0500

    adjust dalli cache store in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,9 @@ Rails.application.configure do
                          password: ENV["MEMCACHIER_PASSWORD"],
                          failover: true,
                          socket_timeout: 1.5,
-                         socket_failure_delay: 0.2 }
+                         socket_failure_delay: 0.2,
+                         down_retry_delay: 60,
+                         pool_size: 5 }
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   config.active_job.queue_adapter = :sidekiq
