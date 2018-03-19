905645e8084af6e0109efff13612e930e5cfbc5d
Jason Haruska <jason@haruska.com>
2018-03-19 10:57:44 -0500

    update script to create sweet 16

--- script/create_sweet_sixteen_pool.rb
+++ script/create_sweet_sixteen_pool.rb
@@ -1,4 +1,4 @@
-sixty_four_tournament = Tournament.find_by name: Time.current.year.to_s + " NCAA March Madness Tournament"
+sixty_four_tournament = Tournament.current.first
 
 tournament = Tournament.create(
   name: Time.current.year.to_s + " NCAA Sweet Sixteen",
@@ -18,11 +18,9 @@ sixty_four_tournament.round_for(3).each do |game|
   end
 end
 
-sixty_four_tournament.pools.each do |previous_pool|
-  invite_code = previous_pool.invite_code
-  previous_pool.update!(invite_code: Pool.generate_unique_code)
-  pool = tournament.pools.create(name: previous_pool.name + " Sweet 16", invite_code: invite_code)
+pool = tournament.pools.create(name: "Sweet Sixteen")
 
+sixty_four_tournament.pools.each do |previous_pool|
   previous_pool.pool_users.each do |pool_user|
     pool.pool_users.create(user_id: pool_user.user_id, role: pool_user.role)
   end
